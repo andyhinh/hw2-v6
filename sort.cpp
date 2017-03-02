a510bd4a43b42720f4ab2a94ccb761b1a13df82a
lcarranco <lcarranco@gmail.com>
2017-03-02 15:47:02 -0600

    Editing merge function
    Did not finish fixing the merge function

--- sort.cpp
+++ sort.cpp
@@ -449,27 +449,35 @@ void merge_sort(BigNumber *a, int p,int r)
         cout<<"Entered merge"<<endl;
         int n1=q-p+1;
         int n2=r-q;
-        BigNumber *L[n1+1];
-        BigNumber *R[n2+1];
+        cout << "Before: BigNumber *L" << endl;
+        BigNumber *L = new BigNumber[n1+1];
+        BigNumber *R = new BigNumber[n2+1];
+        cout << "After: BigNumber *L" << endl;        
+        cout << "Before for-1: " << endl;        
         for(int i=1;i<=n1;i++)
         {
-            *L[i]=a[p+i-1];
+            cout << "*L[i]=a..." << endl;        
+            L[i]=a[p+i-1];
         }
+        cout << "Before for-2: " << endl;
         for(int j=1;j<=n2;j++)
         {
-            *R[j]=a[q+j];
+            cout << "*R[j]=*a[q+j]" << endl;
+            R[j]=a[q+j];
         }
         int i=1, j=1;
+        cout << "Before for-3: " << endl;        
         for(int k=p;k<=r;k++)
         {
+            cout << "if(L[i]<=R[j])" << endl;
             if(L[i]<=R[j])
             {
-                a[k]=*L[i];
+                a[k]=L[i];
                 i=i+1;
             }
             else
             {
-                a[k]=*R[j];
+                a[k]=R[j];
                 j=j+1;
             }
         }
